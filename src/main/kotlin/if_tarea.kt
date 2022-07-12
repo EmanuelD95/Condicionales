c89bab28615aecebb20abb65961734c29f87ca00
Emanuel Muñoz <emanuel95.d@gmail.com>
2022-07-12 09:09:20 -0500

    Tarea

---
+++ src/main/kotlin/if_tarea.kt
@@ -0,0 +1,10 @@
+
+/** TAREA:
+ * Imprimir número de hemanos.
+ * Calcular si es mayor que 5
+ * Calcular si es menor que 10
+ * Calcular si es igual a 9
+ * Calcular si es mayor que 7 y menor que 11
+ * Calcular si es múltiplo de 3
+ * Calcular si es impar // if(hnos%2==1)
+ */
